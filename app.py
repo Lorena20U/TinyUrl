16146deef2c9c29b30e9cec4c6900ac9eb338543
Yaneth33612 <lorenaperez@ufm.edu>
2020-11-17 17:14:34 -0600

    first commit - bases

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, redirect, url_for
+from jinja2 import Template, Environment, FileSystemLoader
+import redis
+
+file_loader = FileSystemLoader('templates')
+env = Environment(loader = file_loader)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    template = env.get_template('layout.html')
+    return template.render()
+
+@app.route('/urls')
+def urlsList():
+    template = env.get_template('urls.html')
+    return template.render()
+
+@app.route('/stats')
+def stat():
+    template = env.get_template('stats.html')
+    return template.render()
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug = True)
